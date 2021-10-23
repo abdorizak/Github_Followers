c70204dc986aa36ac602393f5157428b2d1b29f1
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-23 05:31:09 +0300

    Initial Commit

---
+++ Github Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Followers
+//
+//  Created by Abdirizak Hassan on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
