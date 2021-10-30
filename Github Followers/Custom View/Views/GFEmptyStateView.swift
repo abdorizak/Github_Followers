4eaa15f035a672f0a4efce0913c1747f6363938b
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-30 06:13:18 +0300

    Learning programatic UI

---
+++ Github Followers/Custom View/Views/GFEmptyStateView.swift
@@ -0,0 +1,37 @@
+//
+//  GFEmptyStateView.swift
+//  Github Followers
+//
+//  Created by Abdirizak Hassan on 10/30/21.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+    let messageLabel = GFTitleLabel(textAligment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
