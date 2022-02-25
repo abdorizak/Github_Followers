9e8d5b5704168fe6687066467466989cb43caf99
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-25 09:42:32 +0300

    CustomViews - Views - GFItemInfoView

--- Github Followers/Custom View/Views/GFItemInfoView.swift
+++ Github Followers/Custom View/Views/GFItemInfoView.swift
@@ -57,17 +57,17 @@ class GFItemInfoView: UIView {
     func set(ItemInfoType: itemInfoType, withCount count: Int) {
         switch ItemInfoType {
         case .repos:
-            symbolImageView.image   = UIImage(systemName: SFSymbols.repos)
+            symbolImageView.image   = SFSymbols.repos
             titleLabel.text         = "Public Repos"
             
         case .gists:
-            symbolImageView.image   = UIImage(systemName: SFSymbols.gists)
+            symbolImageView.image   = SFSymbols.gists
             titleLabel.text         = "Public Gists"
         case .followers:
-            symbolImageView.image   = UIImage(systemName: SFSymbols.followers)
+            symbolImageView.image   = SFSymbols.followers
             titleLabel.text         = "Followers"
         case .following:
-            symbolImageView.image   = UIImage(systemName: SFSymbols.following)
+            symbolImageView.image   = SFSymbols.following
             titleLabel.text         = "Following"
         }
         
