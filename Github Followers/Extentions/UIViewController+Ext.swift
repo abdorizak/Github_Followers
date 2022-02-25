10ff11c9c0c56560d0faeca93a8af5a25b7bddb3
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-25 09:43:48 +0300

    Extentions - UIViewController+Ext

--- Github Followers/Extentions/UIViewController+Ext.swift
+++ Github Followers/Extentions/UIViewController+Ext.swift
@@ -8,8 +8,6 @@
 import UIKit
 import SafariServices
 
-fileprivate var containerView: UIView!
-
 extension UIViewController {
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -26,42 +24,6 @@ extension UIViewController {
         let safariVC = SFSafariViewController(url: url)
         safariVC.preferredControlTintColor = .systemGreen
         present(safariVC, animated: true, completion: nil)
-    }
-    
-    func showLoadingview() {
-        containerView = UIView(frame: view.bounds)
-        view.addSubview(containerView)
-        
-        containerView.backgroundColor = .systemBackground
-        containerView.alpha = 0
-        
-        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8  }
-        
-        let activatyIndicator = UIActivityIndicatorView(style: .large)
-        containerView.addSubview(activatyIndicator)
-        
-        activatyIndicator.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            activatyIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            activatyIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-        
-        activatyIndicator.startAnimating()   
-    }
-    
-    func dismissLoding() {
-        DispatchQueue.main.async {
-            containerView.removeFromSuperview()
-            containerView = nil
-        }
-    }
-    
-    
-    func showEmptyStateView(with message: String, in view: UIView) {
-        let emptyStateView = GFEmptyStateView(message: message)
-        emptyStateView.frame = view.bounds
-        view.addSubview(emptyStateView)
     }
     
     
