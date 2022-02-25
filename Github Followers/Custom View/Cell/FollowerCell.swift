748208f6bf6eb75e3107172d36f8b6fcd077f8c1
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-25 09:36:06 +0300

    CustomViews - Cell - FollowerCell

--- Github Followers/Custom View/Cell/FollowerCell.swift
+++ Github Followers/Custom View/Cell/FollowerCell.swift
@@ -25,7 +25,13 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
-        avatarImage.dowloadImage(from: follower.avatar_url)
+        NetworkManger.shared.downloadImage(from: follower.avatar_url) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.avatarImage.image  = image
+            }
+        }
+        
     }
     
     
