59a2dd8c0cceee2a001b45adf6370f6db2047e71
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-25 09:38:28 +0300

    CustomViews - View - Controllers - GFDataLoading

--- Github Followers/Custom View/View Controller/GFDataLoadingVC.swift
+++ Github Followers/Custom View/View Controller/GFDataLoadingVC.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class GFDataLoadingVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var containerView: UIView!
+    func showLoadingview() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8  }
+        
+        let activatyIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activatyIndicator)
+        
+        activatyIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activatyIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activatyIndicator.centerXAnchor.constraint(equalTo: containerView  .centerXAnchor)
+        ])
+        
+        activatyIndicator.startAnimating()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func dismissLoding() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
     }
-    */
+    
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
 
 }
