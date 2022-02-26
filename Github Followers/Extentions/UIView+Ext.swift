658fc204d6784547ebaec822f77ac0e7be8267ac
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-26 15:09:05 +0300

    Extentions - UIView+Ext

--- Github Followers/Extentions/UIView+Ext.swift
+++ Github Followers/Extentions/UIView+Ext.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    
+    func pinToEdges(to superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
+    
+    func addSubViews(_ views: UIView...) {
+        for view in views { addSubview(view )}
+    }
+}
