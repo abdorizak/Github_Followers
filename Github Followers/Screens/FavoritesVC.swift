01f6e8bf3a082aa9359252facd0ca03d2a9bca4a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-26 14:55:18 +0300

    Screens - FavoritesVC

--- Github Followers/Screens/FavoritesVC.swift
+++ Github Followers/Screens/FavoritesVC.swift
@@ -33,6 +33,7 @@ class FavoritesVC: GFDataLoadingVC {
         tableView.rowHeight = 80
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.removeExcessCell()
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.identifier)
     }
     
@@ -79,21 +80,20 @@ extension FavoritesVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let favorite = favorites[indexPath.row]
-        let destVC = FolloweListVC(username: favorite.login)
+        let destVC = FollowerListVC(username: favorite.login)
         navigationController?.pushViewController(destVC, animated: true)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
-        
-        
-        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentGFAlertOnMainThread(title: "Unable To remove", message: error.rawValue, buttonTitle: "ok")
         }
     }
