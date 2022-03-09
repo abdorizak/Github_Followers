774a431000854e5898bf9ae40f55429a59b2ef69
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-09 14:13:21 +0300

    Screens - FollowerListVC

--- Github Followers/Screens/FollowerListVC.swift
+++ Github Followers/Screens/FollowerListVC.swift
@@ -84,16 +84,7 @@ class FollowerListVC: GFDataLoadingVC {
             self.dismissLoding()
             switch result {
             case .success(let followers):
-                if followers.count < 100 { self.hasMoreFollowers = false}
-                self.followers.append(contentsOf: followers)
-                if self.followers.isEmpty {
-                    let message = "This user doesn't have any followers. Go follow them..."
-                    DispatchQueue.main.async {
-                        self.showEmptyStateView(with: message, in: self.view)
-                    }
-                    return
-                }
-                self.updateData(on: self.followers)
+                self.updateUI(with: followers)
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happen", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -101,6 +92,18 @@ class FollowerListVC: GFDataLoadingVC {
         }
     }
     
+    func updateUI(with follower: [Follower]) {
+        if follower.count < 100 { self.hasMoreFollowers = false}
+        self.followers.append(contentsOf: follower)
+        if self.followers.isEmpty {
+            let message = "This user doesn't have any followers. Go follow them..."
+            DispatchQueue.main.async {
+                self.showEmptyStateView(with: message, in: self.view)
+            }
+            return
+        }
+        self.updateData(on: self.followers)
+    }
     
     func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeCulomnFlowLayout(in: view))
